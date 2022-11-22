0ff82db843569aeaf503329f06bcd472d4006cff
Sebastian <43414545+RealSocius@users.noreply.github.com>
2022-11-22 16:23:39 +0100

    Create install-node-nvm.sh

---
+++ install-node-nvm.sh
@@ -0,0 +1,9 @@
+sudo apt install curl 
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.bashrc
+nvm install node
+
+###
+# Uncomment if you want yarn
+###
+# npm install -g yarn
